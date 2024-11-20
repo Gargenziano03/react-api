622e7033273311f4274a252afcdc31c968de62df
Gargenziano03 <gargenziano03@gmail.com>
2024-11-20 16:01:11 +0100

    add function fetchData

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,19 @@ function App() {
     fetchData()
   }
 
+  function fetchData(url = 'http://127.0.0.1:3000/posts') {
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data);
+        setPostsData(data)
+
+      })
+  }
+
   return (
     <>
+      <h1>Blog</h1>
       <button type='button' onClick={handleClick}>fetch posts</button>
     </>
   )
