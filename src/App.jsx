949cc6110b24ad87836a7dbd94deb80c78cbac10
Gargenziano03 <gargenziano03@gmail.com>
2024-11-20 16:40:38 +0100

    add useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
 
@@ -19,7 +19,7 @@ function App() {
       })
   }
 
-
+  useEffect(fetchData, [])
 
   return (
     <>
