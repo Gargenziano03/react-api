8105b133c1912d89ca333b5b9bf54bd631271995
Gargenziano03 <gargenziano03@gmail.com>
2024-11-20 15:54:54 +0100

    add button + function handleClick

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import './App.css'
 function App() {
   const [postsData, setPostsData] = useState({})
 
+  function handleClick(e) {
+    fetchData()
+  }
+
   return (
     <>
-
-
+      <button type='button' onClick={handleClick}>fetch posts</button>
     </>
   )
 }
