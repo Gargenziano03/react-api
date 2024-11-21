b69ad1ac333339c80c95b86190e9f656c370f5a8
Gargenziano03 <gargenziano03@gmail.com>
2024-11-22 00:47:59 +0100

    add image

--- src/App.jsx
+++ src/App.jsx
@@ -27,11 +27,12 @@ function App() {
       <button type='button' onClick={handleClick}>fetch posts</button>
       <section className='posts'>
         <div className="container">
-          <div className="rov row-cols-1 row-cols-md-2 row-cols-lg-3 g-">
+          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-">
             {postsData.data ? postsData.data.map(post => (
               <div className="col" key={post.id}>
                 <div className="card">
                   <h3>{post.title}</h3>
+                  <img width="200px" src={`http://127.0.0.1:3000/${post.image}`} alt="" />
                   <p>{post.content}</p>
                 </div>
               </div>
